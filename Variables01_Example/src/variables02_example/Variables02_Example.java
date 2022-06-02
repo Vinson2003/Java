e71eb3f7c109da6152cb4f690590fe63b3023ec9
Vinson2003 <97678871+Vinson2003@users.noreply.github.com>
2022-06-02 21:34:35 +0700

    section 3

---
+++ Variables01_Example/src/variables02_example/Variables02_Example.java
@@ -0,0 +1,26 @@
+
+package variables02_example;
+
+public class Variables02_Example {
+
+    public static void main(String[] args) {
+        String x = "Vinson Deslian";
+        System.out.println("Haloo!!! Namaku  " +x);
+        System.out.println(x +" adalah seorang cat&dog lovers!");
+        System.out.println("Yo, " +x +"!");
+        System.out.println("Apa kabar, " +x +"-san? "
+                + "Yosh! My name is " +x +". I know how excited "
+                + "everyone is to start appreciating " +x +" on " +x
+                + " Appreciation Day! " +x +", " +x +", " +x +"! Yay "
+                + x +"!!! That's me! " +x +" is the best date ever!");
+
+        String nama = "Alex";
+        nama = "Kira";
+        nama = "Aizawa";
+        
+        //"Yuki" = nama; // can not do this
+        System.out.println(nama);
+        
+    }
+    
+}
---
+++ Variables02/src/variables02/Variables02.java
@@ -0,0 +1,16 @@
+
+package variables02;
+
+
+public class Variables02 {
+
+    public static void main(String[] args) {
+        String x = "puppy";
+        System.out.println(x);
+        x = "kitty";
+        System.out.println(x);
+        x = "bunny";
+        System.out.println(x);
+        
+    }
+}
